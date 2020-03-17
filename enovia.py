c9b97577d11d9aff12fe2a0d903de939cc8dd9bf
StevenFu06 <58870010+StevenFu06@users.noreply.github.com>
2020-03-17 16:11:05 -0400

    Add files via upload

---
+++ enovia.py
@@ -0,0 +1,203 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import wait, expected_conditions as ec
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import TimeoutException
+
+import pickle
+import os
+import time
+import pathlib
+from bs4 import BeautifulSoup
+
+
+class Enovia:
+    """Main enovia api, complete with multithreading/ multiprocessing"""
+
+    ENOVIA_URL = 'http://amsnv-enowebp2.netadds.net/enovia/emxLogin.jsp'
+    TIMEOUT = 10
+
+    XHOME = '//td[@title="Home"]'
+    XSEARCH = '//input[@name="AEFGlobalFullTextSearch"]'
+    XSPEC = '//*[contains(text(),"Specifications")]'
+    XCATEGORY = '//*[@title="Categories"]'
+
+    def __init__(self, username: str, password: str, headless=True):
+        self.username = username
+        self.password = password
+        self.headless = headless
+        self.chrome_options = Options()
+        self.browser = None
+        self.searched = None
+
+    def __enter__(self):
+        self.create_env()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.browser.close()
+        self.browser.quit()
+
+    def close(self):
+        self.browser.close()
+        self.browser.quit()
+
+    def create_env(self):
+        """Sets browser and logs in"""
+
+        # Sets browser & options
+        if self.headless:
+            self.chrome_options.add_argument('--headless')
+        self.browser = webdriver.Chrome(options=self.chrome_options)
+        # Login
+        self.browser.get(self.ENOVIA_URL)
+        self.browser.find_element_by_name('login_name').send_keys(self.username)
+        temp_pass = self.browser.find_element_by_name('login_password')
+        temp_pass.send_keys(self.password)
+        temp_pass.send_keys(Keys.ENTER)
+
+    def reset(self):
+        """Reset back to enovia startup screen"""
+        # For some reason enovia duplicates all elements on startup
+        self.browser.get(self.ENOVIA_URL)  # Browser.get to reset and to avoid any preloaded frames
+
+    def search(self, value: str):
+        """Searches for the value provided in the search"""
+
+        self.searched = value
+        self.reset()
+        search_bar = self._wait(ec.element_to_be_clickable((By.XPATH, self.XSEARCH)))
+        search_bar.clear()
+        search_bar.send_keys(value)
+        search_bar.send_keys(Keys.ENTER)
+        # Puts the new search window into focus
+        self._wait(ec.frame_to_be_available_and_switch_to_it('windowShadeFrame'))
+
+    def open_last_result(self):
+        """Opens the last result in the list regardless of any other conditions
+
+        Note:
+            Need a better way to open search items. I.e. distinguish between revisions,
+            prelim vs proto vs release vs obsolete, etc...
+        """
+        self._wait(ec.frame_to_be_available_and_switch_to_it('structure_browser'))
+
+        part = self._wait(ec.presence_of_all_elements_located((
+            By.XPATH,
+            f'//td[@title={self.searched}]//*[contains(text(),"{self.searched}")]'
+        )))
+        # Javascript click for multiprocessing, normal clicks dont always register
+        self.browser.execute_script("arguments[0].click();", part[-1])
+
+        # Brings the selected part number into focus
+        self.browser.switch_to.default_content()
+        self._wait(ec.frame_to_be_available_and_switch_to_it('content'))
+        self._wait(ec.frame_to_be_available_and_switch_to_it('detailsDisplay'))
+
+    def open_latest_state(self, state):
+        """Opens the last state in search, i.e. last Released
+
+        Note: Open last result is faster ~2 seconds
+        """
+        self._wait(ec.frame_to_be_available_and_switch_to_it('structure_browser'))
+        # Tests to see if the iframe is loaded before getting soup
+        self._wait(ec.presence_of_element_located((By.XPATH, f'//td[@title={self.searched}]')))
+        soup = BeautifulSoup(self.browser.page_source, 'html.parser')
+        # Catch any typo error/ if the state doesnt exist
+        try:
+            object_id = soup.find_all('td', {'title': state})[-1]['rmbid']
+        except IndexError:
+            raise FileNotFoundError(f'{state} for {self.searched} not found')
+
+        part = self.browser.find_element_by_xpath(f'//td[@rmbid="{object_id}"]//a[@class="object"]')
+        # Javascript click for multiprocessing, normal clicks dont always register
+        self.browser.execute_script("arguments[0].click();", part)
+
+        # Brings the selected part number into focus
+        self.browser.switch_to.default_content()
+        self._wait(ec.frame_to_be_available_and_switch_to_it('content'))
+        self._wait(ec.frame_to_be_available_and_switch_to_it('detailsDisplay'))
+
+    def download_specification_files(self, path):
+        """Downloads all files under specifications
+
+        Note:
+            There must be a more direct way to download all the files directly instead of
+            actually going into specifications.
+        """
+        self._enable_download_headless(path)
+        self._wait(ec.element_to_be_clickable((By.XPATH, self.XCATEGORY))).click()
+        self._wait(ec.element_to_be_clickable((By.XPATH, self.XSPEC))).click()
+        # Cannot directly click because after every click, enovia refreshes
+        # After every click download link gets refreshed so needs to get new every time
+        num_downloads = len(self._wait(
+            ec.presence_of_all_elements_located((By.XPATH, '//*[@title="Download"]'))
+        ))
+        for i in range(num_downloads):
+            self._wait(
+                ec.element_to_be_clickable((By.XPATH, f'(//*[@title="Download"])[{i+1}]'))
+            ).click()  # Re-get link based on which is being downloaded now
+            ##############
+            # NEEDS TO CHANGE, is here because download needs to checkout causing delay
+            time.sleep(3)
+            ##############
+            self.wait_until_downloaded(path)
+
+    def _wait(self, expected_condition):
+        """Expected conditions wrapper"""
+
+        try:
+            return wait.WebDriverWait(self.browser, self.TIMEOUT).until(expected_condition)
+        except TimeoutException:
+            raise TimeoutException(f'Timeout at step {expected_condition.__dict__}')
+
+    def _enable_download_headless(self, download_dir):
+        """Enables headless download
+        I have no idea what this does but it allows headless downloads, and I am forever greatfull
+        for the person who has had to sit there and figure this out
+        """
+        self.browser.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+        params = {
+            'cmd': 'Page.setDownloadBehavior',
+            'params': {
+                'behavior': 'allow',
+                'downloadPath': download_dir
+            }
+        }
+        self.browser.execute("send_command", params)
+
+    def wait_until_downloaded(self, download_path):
+        """Waits until download is finished before continuing"""
+
+        def is_finished(path):
+            all_files = os.listdir(path)
+            for file in all_files:
+                if pathlib.Path(file).suffix == '.crdownload':
+                    return False
+            return True
+
+        stahp = time.time() + self.TIMEOUT
+        while not is_finished(download_path):
+            if time.time() >= stahp:
+                raise TimeoutException('Something went wrong while downloading')
+
+
+if __name__ == '__main__':
+
+    with open('credentials', 'rb') as read:
+        cred = pickle.load(read)
+
+    from concurrent.futures import ThreadPoolExecutor
+
+    def multithreading(part):
+        enovia = Enovia(cred['user'], cred['pass'], headless=True)
+        with enovia as enovia:
+            enovia.search(part)
+            enovia.open_last_result()
+            enovia.download_specification_files(os.getcwd())
+
+    test_list = ['5068482', '5068249', '5068248', '5074524']
+    with ThreadPoolExecutor(max_workers=len(test_list)) as executor:
+        for result in executor.map(multithreading, test_list):
+            pass
---
+++ package.py
@@ -0,0 +1,301 @@
+"""Package Creator for CCL
+
+Date: 2020-03-10
+Revision: A
+Author: Steven Fu
+"""
+
+
+import pandas as pd
+from docx.api import Document
+import re
+
+from zipfile import ZipFile
+import json
+import copy
+
+import shutil
+import os
+
+
+class Parser:
+    """Parse the CCL into a DataFrame
+
+    Notes:
+        CCL will be used for the rest of the tool. Any edits to the filtered CCL will
+        be reflected throughout the rest of the program.
+
+    IMPORTANT!!!!!
+    CCL HAS TO BE IN THE FORMAT OF THE 2020 TRF FORMAT (TRF REV M)
+    *****
+
+    Attrs:
+        word_doc: the path to the word document
+        document: the raw word document that was read
+        table: the table object within the word document
+
+    Functions:
+        to_dataframe: Convert word directly to a dataframe
+        filter: Will filter out the important information from the table
+    """
+    def __init__(self, word_doc):
+        self.document = Document(word_doc)
+        self.table = self.document.tables[0]
+
+    def to_dataframe(self):
+        """Convert word directly to dataframe with no regex/ filtering
+
+        Notes:
+            Bold heading are the bolded cable entries in the TRF. Will be mainly used to
+            create the sub folders/ folder hierarchy for the CCL documents
+
+        :return dataframe with columns: columns and data of the raw table document
+        """
+        columns = ['pn', 'desc', 'vendor', 'model', 'technical', 'standards', 'marks', 'bold']
+        to_df = [
+            [cell.text.strip() for cell in row.cells] +
+            [True if row.cells[0].paragraphs[0].runs[0].bold else False]
+            for row in self.table.rows
+        ]
+        return pd.DataFrame(data=to_df, columns=columns)
+
+    def filter(self):
+        """Filter the inputted table into useful data only
+
+        Note:
+            Nums are ints where possible, the rest are string. All regex is done by the
+            static method _re_get_cols. Please see for more information.
+
+        :return DataFrame with columns: pn, desc, fn, assy, sch, bold.
+        """
+        def remove_duplicates(df):
+            """Will only remove duplicates that are touching
+
+            Main use is to remove/ clean up the original merged data.
+            Input is the filtered dataframe.
+            """
+            pointer1, pointer2 = 0, 1
+            while pointer2 < len(df.index):
+                if df.loc[pointer1].equals(df.loc[pointer2]):
+                    df = df.drop([pointer1])
+                pointer1 = pointer2
+                pointer2 += 1
+            return df.reset_index(drop=True)
+
+        columns = ['pn', 'desc', 'fn', 'dnums', 'assy', 'sch']
+        df = self.to_dataframe()
+        data = [Parser._re_getcols(row) for index, row in df.iterrows()]
+        filtered = pd.concat([pd.DataFrame(data, columns=columns), df['bold']], axis=1)
+        return remove_duplicates(filtered)
+
+    @staticmethod
+    def _re_getcols(series: pd.Series):
+        """Use regex to filter out data from series"""
+
+        desc, fn = _re_fn_name(series['desc'])
+        assy, sch = _re_ill(series['technical'])
+        output = [
+            _re_pn(series['pn']),
+            desc,
+            fn,
+            _re_doc_num(series['technical']),
+            assy,
+            sch
+        ]
+        return output
+
+
+def _re_pn(string):
+    """Get the part number"""
+
+    if len(string) <= 7:  # For part numbers not in format of 5XXXXXX
+        return int(string)
+    else:
+        # Find any/ all digits with format of 6 concurrent numbers
+        num = re.findall(r'\d\d\d\d\d\d\d', string)
+        return int(num[0]) if num else None
+
+
+def _re_fn_name(string):
+    """Find function number"""
+
+    # Looks for (#X) as regex
+    fn = re.findall(r'\(#(\+\d+)\)', string)
+    if fn:
+        to_pop = f'(#{fn[0]})'
+        string = string.replace(to_pop, '').strip().replace('\n', '')
+        # After removing the function number from string, needs to remove all
+        # non A-Z 0-9 characters
+        string = re.sub(r'([^\s\w]|_)+', '', string)
+    return string, int(fn[0]) if fn else None
+
+
+# def _re_ill(string):
+#     """Finds illustration number"""
+#
+#     if re.findall(r'refertoill.', string.replace(' ', ''), re.IGNORECASE):
+#         assy = re.findall(r'assy', string.replace(' ', ''), re.IGNORECASE)
+#         sch = re.findall(r'sch', string.replace(' ', ''), re.IGNORECASE)
+#         return True if assy else False, True if sch else False
+#     return False, False
+
+def _re_ill(string):
+    """Finds Illustration required and illustration number"""
+    re.sub(r'\W+', '', string)
+    string = string.replace(' ', '')
+    if re.findall(r'refertoill.', string, re.IGNORECASE):
+        assy = re.findall(r'(\d+)assy', string, re.IGNORECASE)
+        sch = re.findall(r'(\d+)sch', string, re.IGNORECASE)
+        return assy[0] if assy else False, sch[0] if sch else False
+    return False, False
+
+
+def _re_doc_num(string):
+    """Finds document number, same logic as pn but with D in front"""
+
+    dnums = re.findall(r'D\d+', string)
+    return dnums if dnums else None
+
+
+class Parent:
+    """Unencodes the level information on the AVL multi-level BOM
+
+    Note:
+        The AVL multi level BOM must have atleast the "Level" and "Name" for this class to work
+
+    Attributes:
+        bom: the avl multi level bom as a DataFrame
+        lowest: the lowest child (highest level) in the bom
+        flat: the flattened dictionary
+        tree: a multi level dictionary
+    """
+    def __init__(self, avl_bom):
+        self.bom = pd.read_csv(avl_bom)
+        self.lowest = self.bom['Level'].max()
+        self.flat = {}
+        self.tree = {}
+
+    def build_flat(self):
+        """Builds a flat dictionary with parent + child as keys
+
+        Note:
+            build_flat does not modify the original bom.
+
+        :return flat: will assign the flat attribute a dictionary containing ALL parents as keys
+                    in AVL bom with child listed as list
+        """
+        # Uses 2 pointers to check if next part is parent of top
+        parent, child, parent_idx = 1, 2, None
+        # Will check 1 level at a time starting with 1 ending with the lowest level
+        while parent < self.lowest:
+            # Will scan through entire BOM for every new level
+            for idx in self.bom.index:
+
+                if self.bom.loc[idx, 'Level'] == parent:
+                    parent_idx = f'{idx} {self.bom.loc[idx, "Name"]}'
+                    self.flat[parent_idx] = {}
+
+                elif self.bom.loc[idx, 'Level'] == child and parent_idx is not None:
+                    self.flat[parent_idx][f'{idx} {self.bom.loc[idx, "Name"]}'] = {}
+
+            parent += 1  # Search for next level down parent
+            child += 1
+        return self.flat
+
+    def build_tree(self):
+        """Returns a structured tree using the flattened one
+
+        Using the flat dictionary, it will reassemble it using the idea that there will be repeat
+        keys. The child will now become the parent of the individual parent. For example, if
+        12:{1,2,3,4}, and 15:{10, 11, 12, 13, 14} --> 15:{10, 11, 12:{1,2,3,4}, 13, 14}.
+
+        After the child gets set, it will pop a key from a copy of the flattened list. Repeat until copied
+        list is empty.
+
+        :return dictionary with same list level as Enovia BOM
+        """
+        if not self.flat:
+            self.build_flat()
+        # Creates a copy of the flat attribute for popping
+        copy_flat = copy.deepcopy(self.flat)
+        while copy_flat:
+            key = list(copy_flat)[0]
+            # If empty, ie the first run, will set first key as first parent
+            # This is ok because the flattened tree is in the same order as the bom.
+            if not self.tree:
+                self.tree[key] = copy_flat.pop(key)
+                continue
+            data = copy_flat.pop(key)
+            Parent.replace_item(self.tree, key, data)
+            # Checks if an item was inserted, if not the data will be appended to end
+            if not Parent.exists(self.tree, key):
+                self.tree[key] = data
+        return self.tree
+
+    @staticmethod
+    def replace_item(obj, key, replace_value):
+        """Recursively replaces item/ inserts item"""
+
+        for k, v in obj.items():
+            if v:
+                obj[k] = Parent.replace_item(v, key, replace_value)
+        if key in obj:
+            obj[key] = replace_value
+        return obj
+
+    @staticmethod
+    def exists(obj, key):
+        """Recursively checks if item exists in dictionary"""
+
+        if key in obj:
+            return True
+        for k, v in obj.items():
+            if Parent.exists(v, key):
+                return True
+
+
+def BuildPackage(save_path: os.path, word_doc: os.path, avl_bom: os.path):
+    """Builds the CCL package using Parser and Parent classes
+
+    Parameters:
+        :param save_path: the path/ location of where to save the CCL package
+        :param word_doc: path/ location of where the CCL is saved (docx)
+        :param avl_bom: path/ location of where the AVL multi level bom is saved (csv)
+
+    :return a zip file at the location of the save path
+    """
+    if os.path.exists(save_path):
+        raise FileExistsError('File with same name already exists in folder')
+
+    parent = Parent(avl_bom)
+    parse = Parser(word_doc)
+
+    try:
+        # Create a temporary folder where all files are waiting to be zipped
+        # Temporary folder is created in the cwd
+        os.makedirs('temp')
+        parse.filter().to_csv(os.path.join('temp', 'filter.csv'))
+        parse.document.save(os.path.join('temp', 'ccl.docx'))
+        parent.bom.to_csv(os.path.join('temp', 'bom.csv'))
+        with open(os.path.join('temp', 'flat.json'), 'w') as write:
+            json.dump(parent.build_flat(), write, indent=4)
+        with open(os.path.join('temp', 'tree.json'), 'w') as write:
+            json.dump(parent.build_tree(), write, indent=4)
+        # Zip the folder with all its contents
+        with ZipFile(save_path, 'w') as package:
+            files = os.listdir('temp')
+            for file in files:
+                package.write(os.path.join('temp', file), file)
+    except Exception as e:
+        raise e
+    finally:
+        # Remove the temporary folder
+        shutil.rmtree('temp')
+
+
+if __name__ == '__main__':
+    path = 'C:\\Users\\steven.fu\\Documents\\CCL Builder\\Workspace'
+    word_doc = os.path.join(path, 'Wombat CCL.docx')
+    avl_bom = os.path.join(path, 'revg.csv')
+    #
+    # BuildPackage('ccl package_Rev G-SAMPLE_take3.zip', word_doc, avl_bom)
