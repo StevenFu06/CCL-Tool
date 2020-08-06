beace8db11efa08f91e1f665a43ef6eb3797bae8
Steven Fu <steven.fu373@gmail.com>
2020-08-06 15:40:07 -0400

    Entry input on back

--- compare.py
+++ compare.py
@@ -25,7 +25,6 @@ class Bom:
         parent_list (list): parent attribute but as a list for easy iter
         top_pn (list): Part numbers at highest level
     """
-
     def __init__(self, avl_bom, parent):
         self.bom = avl_bom
         self.parent = parent
@@ -112,7 +111,19 @@ class Bom:
 
 
 class Tracker:
+    """Tracker class used to track updates
 
+    Tracker is used in to track the updates during the recursive process of "update" and "rearrange"
+
+    Attributes:
+        full_match (dataframe): pandas dataframe that contains all parts found to be full match
+        partial_match (dataframe): pandas dataframe that contains all parts found to be partial match
+        find_only (dataframe): pandas dataframe that contains all parts found to be find only match
+        not_found (set): a list of unique not found part numbers
+        used (list): a list of already scanned part numbers to prevent duplicates
+
+    All dataframes contain the same column headers for easy data manipulation as shown in COLUMNS
+    """
     COLUMNS = ['old_idx', 'old_pn', 'new_idx', 'new_pn']
 
     def __init__(self):
@@ -123,33 +134,47 @@ class Tracker:
         self.used = []
 
     def append_full(self, part_old, part_new):
+        """Append full match parts to the full_match dataframe"""
+
         self.used.append(part_new)
         append = pd.DataFrame([part_old + part_new],
                               columns=self.COLUMNS)
         self.full_match = pd.concat([self.full_match, append], ignore_index=True)
 
     def append_partial(self, part_old, part_new):
+        """Append partial match parts to the partial_match dataframe"""
+
         self.used.append(part_new)
         append = pd.DataFrame([part_old + part_new],
                               columns=self.COLUMNS)
         self.partial_match = pd.concat([self.partial_match, append], ignore_index=True)
 
     def append_find_only(self, part_old, part_new):
+        """Appaned matched parts to the find_only dataframe"""
+
         append = pd.DataFrame([part_old + part_new],
                               columns=self.COLUMNS)
         self.find_only = pd.concat([self.find_only, append], ignore_index=True)
 
     def not_found_to_df(self):
+        """Convers the not_found list to a dataframe"""
+
         df = pd.DataFrame(data=self.not_found, columns=['idx', 'pn'])
         return df
 
     def isused(self, part):
+        """Check if part has been used"""
+
         return True if part in self.used else False
 
     def reset_not_found(self):
+        """Resets not found set"""
+
         self.not_found = set()
 
     def combine_found(self):
+        """Combines all match types into one and inserts match type column"""
+
         self.full_match.insert(4, 'match_type', 'full')
         self.partial_match.insert(4, 'match_type', 'partial')
         self.find_only.insert(4, 'match_type', 'fn_only')
@@ -157,7 +182,25 @@ class Tracker:
         return combined.sort_values(by=['old_idx'])
 
 
-def ismatch(bom_old, part_old, bom_new, part_new, threshold_full=50, threshold_partial=80):
+def ismatch(bom_old: Bom, part_old: tuple, bom_new: Bom, part_new: tuple,
+            threshold_full: int = 50, threshold_partial: int = 80):
+    """Determines the match type given a part and BOM
+
+    There are 3 types match types in total, 2 are determined in this method (full match, partial match)
+    Conditions:
+        full: find number match & name/ description must be more than 50% similar
+        partial: find number doesnt need to match, description/ name must be more than 80% similar
+
+    Parameters:
+        :param bom_old: bom object of the old bom
+        :param part_old: list following the format of .split in bom class for the old part
+        :param bom_new: bom object of the new bom
+        :param part_new: list following the format of.split in bom class for suspected new/ updated part
+        :param threshold_full: the description match percentage for full match
+        :param threshold_partial: the description match for percentage for partial match
+
+    :return: full for full match, partial for partial match, False for no match
+    """
     old_fn, new_fn = bom_old.bom.loc[part_old[0], 'F/N'], bom_new.bom.loc[part_new[0], 'F/N']
 
     old_desc, new_desc = bom_old.bom.loc[part_old[0], 'Description'], bom_new.bom.loc[part_new[0], 'Description']
@@ -172,10 +215,24 @@ def ismatch(bom_old, part_old, bom_new, part_new, threshold_full=50, threshold_p
     return False
 
 
-def update_part(bom_old, part_old, bom_new, exclusive_new, tracker):
+def update_part(bom_old: Bom, part_old: tuple, bom_new: Bom, exclusive_new: list, tracker: Tracker):
+    """Using ismatch function, formats and determines the updated parts
+
+    This method is mainly used for formating purposes. Will populate the tracker class by determining
+    the match type using ismatch.
+
+    Parameter:
+        :param bom_old: bom object of the old bom
+        :param part_old: list following the format of .split in bom class for the old part
+        :param bom_new: bom object of the new bom
+        :param exclusive_new: list of part number that contain parts exclusive to the new bom
+        :param tracker: tracker class used to track recursion results
+
+    :return: Nothing, will simply update the tracker
+    """
+    # Run once through once for full match
     for part_new in exclusive_new:
         match_status = ismatch(bom_old, part_old, bom_new, part_new)
-
         if match_status == 'full' and not tracker.isused(part_new):
             print(f'{part_old[1]} {bom_old.bom.loc[part_old[0], "Description"]} updated to '
                   f'{part_new[1]} {bom_new.bom.loc[part_new[0], "Description"]}')
@@ -184,7 +241,7 @@ def update_part(bom_old, part_old, bom_new, exclusive_new, tracker):
             bom_old.parent[f'{part_old[0]} {part_new[1]}'] = \
                 bom_old.parent.pop(f'{part_old[0]} {part_old[1]}')
             return
-
+    # Run again through for partial match
     for part_new in exclusive_new:
         match_status = ismatch(bom_old, part_old, bom_new, part_new)
         if match_status == 'partial' and not tracker.isused(part_new):
@@ -200,18 +257,30 @@ def update_part(bom_old, part_old, bom_new, exclusive_new, tracker):
     return
 
 
-def Update(bom_old, bom_new, tracker):
+def Update(bom_old: Bom, bom_new: Bom, tracker: Tracker):
     """Main updater class without rearrange
+
+    Recurssivly scans through the BOM only comparing parent and child parts, not entire BOM and not child of child
+    (Only scans a single level). After determining updates, will update the tracker class.
 
     All bom will be dealt with using the Bom class. This means the index and pn have been already split.
     In the form [index, part number]
 
-    ## mention index numbers somewhere ##
+    Index numbers refer to the pandas index numbers (automaticallyassigned by pandas).
+
+    Parameter:
+        :param bom_old: Bom object for the old bom
+        :param bom_new: bom object for the new bom
+        :param tracker: tracker for updating parts
+
+    :return: nothing, will simply update the tracker class
     """
+    # Creates exclusive lists for easy iteration, uses __sub__ inside of Bom
     exclusive_old = bom_old - bom_new
     exclusive_new = bom_new - bom_old
-
     # Checks to see if only find number has been changed
+    # Will check for matching part numbers, disregarding find number (F/N)
+    # Creates a deep copy for manipulation
     deep_copy_parent = deepcopy(bom_new.parent_list)
     deep_copy_top = deepcopy(bom_new.top_pn)
     for part in bom_old.parent:
@@ -219,23 +288,25 @@ def Update(bom_old, bom_new, tracker):
         if key_old not in exclusive_old:
             try:
                 index = deep_copy_top.index(key_old[1])
-
                 key_new = Bom._split_key(deep_copy_parent.pop(index))
                 del deep_copy_top[index]
-
                 if bom_old.bom.loc[key_old[0], 'F/N'] != bom_new.bom.loc[key_new[0], 'F/N']:
                     tracker.append_find_only(key_old, key_new)
             except ValueError:
+                # Any errors occur means part was not found and needs review
                 tracker.not_found.add(key_old)
-
+    # Finds intersection aka parts that have not been changed in the newer revision
     tracker.used = tracker.used + bom_new.intersect(bom_old)
-
+    # Run through old parts in exclusive_old to see if any needs to be updated
     for part_old in exclusive_old:
         try:
             update_part(bom_old, part_old, bom_new, exclusive_new, tracker)
         except KeyError:
+            # Any errors occur means part was not found and needs review
             tracker.not_found.add(part_old)
-
+    # Recursive formula for going down the BOM parents.
+    # Updated parts will be assigned here to continue the update without needing
+    # to redo the entire process
     for part_old, part_new in Bom.zip_intersect(bom_old, bom_new):
         if bom_old.parent[f'{part_old[0]} {part_new[1]}']:
             next_iter_old = Bom(bom_old.bom, bom_old.parent[f'{part_old[0]} {part_new[1]}'])
@@ -244,7 +315,9 @@ def Update(bom_old, bom_new, tracker):
     return bom_old.parent
 
 
-def insert(obj, key, new_key, new_value):
+def insert(obj: dict, key: int, new_key: int, new_value: str):
+    """Recursive inserts the new value into dictionary"""
+
     for k, v in obj.items():
         if v:
             obj[k] = insert(v, key, new_key, new_value)
